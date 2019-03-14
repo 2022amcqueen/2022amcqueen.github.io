e574637bc91fe0c41a222d4d4678e4feaf3fdf63
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-03-14 14:51:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function summation(start, stop, f) {
+    let x = 0;
+    
+    for (let i = start; i < stop; i++) {
+        let y = f(i)
+        
+        x += y
+    }
+    
+    return y
+}
+
+function bbpFormula(digit) {
+    let pi = summation(0, digit, function(i) {
+        ((1 / (16^i)) * ((4 / ((8 * i) + 1)) - (2 / ((8 * i) + 4)) - (1 / ((8 * i) + 5)) - (1 / ((8 * i) + 6))))
+    })
+    
+    Console.log(pi)
+}
+
+bbpFormula(100)
