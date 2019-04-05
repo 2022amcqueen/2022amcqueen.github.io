38c1a7d3b397743d710f6eefdb9989e2dc3a9cc2
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-04-05 08:54:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ function summation(start, stop, f) {
     let x = 0;
     
     for (let i = start; i < stop; i++) {
-        let y = f(i)
+        let y = f(i); 
         
-        x += y
+        x += y; 
     }
     
-    return x
+    return x; 
 }
 
 function bbpFormula(digit) {
     let pi = summation(0, digit, function(i) {
-        ((1 / (16^i)) * ((4 / ((8 * i) + 1)) - (2 / ((8 * i) + 4)) - (1 / ((8 * i) + 5)) - (1 / ((8 * i) + 6))))
-    })
+        ((1 / (16^i)) * ((4 / ((8 * i) + 1)) - (2 / ((8 * i) + 4)) - (1 / ((8 * i) + 5)) - (1 / ((8 * i) + 6)))); 
+    }); 
     
-    alert(pi)
+    alert(pi); 
 }
 
-bbpFormula(100)
+bbpFormula(100); 
